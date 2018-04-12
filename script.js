8b3287be15aeb99f0af9b41fe69adf75985d9305
Nikolai <meta1-heart@yandex.ru>
2018-04-12 21:53:47 +0500

    UI update
    Use control panel to manage the parameters

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 var canvas, context;
 var HEIGHT = window.innerHeight, WIDTH = window.innerWidth;
 const pi =  Math.PI;
-var segmentAmount = 3;
-var rotationSpeed = 0;
+var rotationLength = 0;
 var dt = 1;
-var colors =  ["#ff0000", "#00ff00", "#0000ff"];
 var isStarted = false;
 var isPaused = false;
+var isFastEnough = false;
 var anim;
-var dw = 0;
 
-document.addEventListener("DOMContentLoaded", main);
+var Circle = function() {
+    this.segments = 3;
+    this.speed = 0;
+    this.color1 =  "#ff0000";
+    this.color2 =  "#00ff00";
+    this.color3 =  "#0000ff";
+    this.updateColors = function() {
+        this.colors =  [this.color1, this.color2, this.color3];
+    };
+};
+
 document.addEventListener("keydown", onKeyDown);
-
+document.addEventListener("DOMContentLoaded", main);
 function onKeyDown(/*KeyDownEvent*/ e) {
 
     if (e.keyCode == 69) { // E
@@ -36,10 +44,10 @@ function pause() {
     }
 }
 function addSpeed() {
-    dw += 0.01; 
+    circle.speed += 0.01; 
 }
 function decSpeed() {
-    dw -= 0.01; 
+    circle.speed -= 0.01; 
 }
 
 
@@ -47,7 +55,10 @@ function main() {
     if (!isStarted){
         prepareCanvas();
         isStarted = true;
+        circle = new Circle();
+        setupGui();
     }
+    circle.updateColors();
     Update();
     Draw();
     anim = requestAnimationFrame(main);
@@ -61,8 +72,21 @@ function prepareCanvas() {
     context = canvas.getContext("2d");
 }
 
+function setupGui() {
+    var gui = new dat.GUI();
+    gui.add(circle, 'segments', 3, 99)
+        .name("Segment amount")
+        .step(3);
+    gui.add( circle, 'speed', -7, 7 )
+        .name("Rotation speed")
+        .step(0.001);
+    gui.addColor(circle, 'color1');
+    gui.addColor(circle, 'color2');
+    gui.addColor(circle, 'color3');
+}
+
 function Update() {
-    rotationSpeed += dw;
+    rotationLength += circle.speed / dt;
 }
 
 function Draw() {
@@ -75,16 +99,16 @@ function Draw() {
     let y = HEIGHT * 0.5;
     let r = 200;
 
-    for (let i = 0; i < segmentAmount; i++) {
-        let startAngle = i * 2 * pi / segmentAmount + rotationSpeed*dt;
-        let endAngle = (i + 1) * 2 * pi / segmentAmount + rotationSpeed*dt;
+    for (let i = 0; i < circle.segments; i++) {
+        let startAngle = i * 2 * pi / circle.segments + rotationLength / dt;
+        let endAngle = (i + 1) * 2 * pi / circle.segments + rotationLength / dt;
         let j = i % 3;
         context.beginPath();
         context.moveTo(x, y);
         context.arc(x, y, r, startAngle, endAngle);
         context.lineTo(x,y);
         context.closePath();
-        context.fillStyle = colors[j];
+        context.fillStyle = circle.colors[j];
         context.fill();
     }
 }
